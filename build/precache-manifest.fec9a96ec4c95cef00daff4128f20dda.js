d9d86bdab040d7851f4f25f9d1593f43c28c360b
Miika <mhtoiv@utu.fi>
2020-10-19 20:57:17 +0300

    frontend added

---
+++ build/precache-manifest.fec9a96ec4c95cef00daff4128f20dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6f8cc1975a511f8c2c55e0ede9586c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f807c5be5a884a6ee74",
+    "url": "/static/css/main.8310aaf5.chunk.css"
+  },
+  {
+    "revision": "c1015d948e35500d5991",
+    "url": "/static/js/2.49709293.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/static/js/2.49709293.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f807c5be5a884a6ee74",
+    "url": "/static/js/main.bb9398d6.chunk.js"
+  },
+  {
+    "revision": "f14b53a185d90ca40d92",
+    "url": "/static/js/runtime-main.b711408c.js"
+  }
+]);
