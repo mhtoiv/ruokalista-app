ac3f7396f427e67a8f428b5bc49d45152a7057cd
Miika <mhtoiv@utu.fi>
2020-10-26 19:40:26 +0200

    fixing...

---
+++ build/precache-manifest.d000dc151b2d81a2653d5781dd2d9e60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020b007f27d0ebb104b05e3532740cc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3901d59c09d2ffce6bc",
+    "url": "/static/css/main.2f723b9f.chunk.css"
+  },
+  {
+    "revision": "9847ff69bf7c45be1d9d",
+    "url": "/static/js/2.fdb20a9f.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/static/js/2.fdb20a9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3901d59c09d2ffce6bc",
+    "url": "/static/js/main.200ac1dc.chunk.js"
+  },
+  {
+    "revision": "f14b53a185d90ca40d92",
+    "url": "/static/js/runtime-main.b711408c.js"
+  }
+]);
